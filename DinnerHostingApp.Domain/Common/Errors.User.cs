bbae89138247d478b3f4a45fddae04871fecbe3d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-01 16:14:15 +0300

    added

---
+++ DinnerHostingApp.Domain/Common/Errors.User.cs
@@ -0,0 +1,11 @@
+using ErrorOr;
+
+namespace DinnerHostingApp.Domain.Common;
+
+public static partial class Errors
+{
+    public static class User
+    {
+        public static Error DuplicateEmail => Error.Conflict(code: "User.DuplicateEmail", description: "Email is already in use!");
+    }
+}
