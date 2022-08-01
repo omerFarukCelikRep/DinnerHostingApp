9460e25a730c08793640b2b0e84984663b88b7b5
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-01 18:17:48 +0300

    added

---
+++ DinnerHostingApp.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,46 @@
+using DinnerHostingApp.Application.Authentication.Common;
+using DinnerHostingApp.Application.Common.Interfaces.Authentication;
+using DinnerHostingApp.Application.Common.Interfaces.Persistence;
+using DinnerHostingApp.Domain.Common;
+using DinnerHostingApp.Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace DinnerHostingApp.Application.Authentication.Commands.Register;
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public RegisterCommandHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand command, CancellationToken cancellationToken)
+    {
+         //Validate the user doesn't exist
+        if (_userRepository.GetByEmail(command.Email) is not null)
+        {
+            return Errors.User.DuplicateEmail;
+        }
+
+        //Create user (generate unique Id) && Persist the DB
+        var user = new User
+        {
+            FirstName = command.FirstName,
+            LastName = command.LastName,
+            Email = command.Email,
+            Password = command.Password
+        };
+
+        _userRepository.Add(user);
+
+        //Create Jwt Token
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
+        return new AuthenticationResult(user, token);
+    }
+}
