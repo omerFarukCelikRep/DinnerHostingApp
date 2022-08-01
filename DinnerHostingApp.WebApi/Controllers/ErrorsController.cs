8ec2522ea463eeca93cdc817d0222312aa2e95fd
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-08-01 16:16:00 +0300

    Error action refactoring

--- DinnerHostingApp.WebApi/Controllers/ErrorsController.cs
+++ DinnerHostingApp.WebApi/Controllers/ErrorsController.cs
@@ -1,15 +1,12 @@
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DinnerHostingApp.WebApi.Controllers;
 
-public class ErrorsController : ControllerBase
+public class ErrorsController : ApiController
 {
     [Route("/error")]
     public IActionResult Error()
     {
-        var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-
-        return Problem(title : exception?.Message, statusCode:400);
+        return Problem(statusCode: StatusCodes.Status500InternalServerError, title: "An unexpected error occured!");
     }
 }
