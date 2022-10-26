07f2adbe0704fd155d2dc3ee1317b5de5d7c58de
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-10-26 23:22:38 +0300

    added

---
+++ DinnerHostingApp.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,6 @@
+namespace DinnerHostingApp.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id) { }
+}
