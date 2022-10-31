7206eb0c2a34922bb807d1cd45e314fd7f109d81
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-01 01:20:41 +0300

    added

---
+++ DinnerHostingApp.Domain/MenuReview/ValueObjects/MenuReviewId.cs
@@ -0,0 +1,22 @@
+using DinnerHostingApp.Domain.Common.Models;
+
+namespace DinnerHostingApp.Domain.MenuReview.ValueObjects;
+
+public sealed class MenuReviewId : ValueObject
+{
+    public Guid Value { get; }
+    private MenuReviewId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static MenuReviewId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
