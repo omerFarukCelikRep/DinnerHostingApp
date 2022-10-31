398414d620cfff039720d42ef552b6c7de895f82
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-01 01:20:32 +0300

    added

---
+++ DinnerHostingApp.Domain/Menu/ValueObjects/MenuSectionId.cs
@@ -0,0 +1,21 @@
+using DinnerHostingApp.Domain.Common.Models;
+
+namespace DinnerHostingApp.Domain.Menu.ValueObjects;
+
+public sealed class MenuSectionId : ValueObject
+{
+    public Guid Value { get; }
+    private MenuSectionId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static MenuSectionId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
