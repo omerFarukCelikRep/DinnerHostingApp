735ccb26281c558df33e4e3740ace1f6701b304d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-01 01:18:54 +0300

    added

---
+++ DinnerHostingApp.Domain/Bill/ValueObjects/BillId.cs
@@ -0,0 +1,21 @@
+using DinnerHostingApp.Domain.Common.Models;
+
+namespace DinnerHostingApp.Domain.Bill.ValueObjects;
+
+public sealed class BillId : ValueObject
+{
+    public Guid Value { get; }
+    private BillId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static BillId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
