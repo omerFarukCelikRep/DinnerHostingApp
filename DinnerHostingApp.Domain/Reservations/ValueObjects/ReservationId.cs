1b122b2ab2f4abf4da2c58ea84f21ea9d29d662d
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-01 23:20:53 +0300

    added

---
+++ DinnerHostingApp.Domain/Reservations/ValueObjects/ReservationId.cs
@@ -0,0 +1,21 @@
+using DinnerHostingApp.Domain.Common.Models;
+
+namespace DinnerHostingApp.Domain.Reservations.ValueObjects;
+
+public sealed class ReservationId : ValueObject
+{
+    public Guid Value { get; }
+    private ReservationId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static ReservationId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
