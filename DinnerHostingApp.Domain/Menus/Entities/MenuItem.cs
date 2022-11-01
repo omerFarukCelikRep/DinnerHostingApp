1df0762b336d989c122800b515edb040a4660190
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-01 23:17:48 +0300

    added

---
+++ DinnerHostingApp.Domain/Menus/Entities/MenuItem.cs
@@ -0,0 +1,20 @@
+using DinnerHostingApp.Domain.Common.Models;
+using DinnerHostingApp.Domain.Menus.ValueObjects;
+
+namespace DinnerHostingApp.Domain.Menus.Entities;
+
+public sealed class MenuItem : Entity<MenuItemId>
+{
+    public string Name { get; }
+    public string Description { get; }
+    private MenuItem(MenuItemId menuItemId, string name, string description) : base(menuItemId)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public static MenuItem Create(string name, string description)
+    {
+        return new(MenuItemId.CreateUnique(), name, description);
+    }
+}
