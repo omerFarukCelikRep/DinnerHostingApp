90b9679ff163e4c2f6fb10563c012641d265fd8c
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-27 16:54:09 +0300

    added

---
+++ DinnerHostingApp.Application/Menus/Create/CreateMenuCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace DinnerHostingApp.Application.Menus.Create;
+public class CreateMenuCommandValidator :AbstractValidator<CreateMenuCommand>
+{
+    public CreateMenuCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.Sections).NotEmpty();
+    }
+}
