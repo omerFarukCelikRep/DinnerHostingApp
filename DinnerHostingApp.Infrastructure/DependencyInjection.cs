36d7a954e1ec54faf0550774a44f1fedd311cad1
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-27 16:58:36 +0300

    MenuRepository lifetime added

--- DinnerHostingApp.Infrastructure/DependencyInjection.cs
+++ DinnerHostingApp.Infrastructure/DependencyInjection.cs
@@ -17,11 +17,21 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
-        services.AddAuthentication(configuration);
+        services
+            .AddAuthentication(configuration)
+            .AddPersistance();
 
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
+       
+
+        return services;
+    }
+
+    public static IServiceCollection AddPersistance(this IServiceCollection services)
+    {
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IMenuRepository, MenuRepository>();
 
         return services;
     }
