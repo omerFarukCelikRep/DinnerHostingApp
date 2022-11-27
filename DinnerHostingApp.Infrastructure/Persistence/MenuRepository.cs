732fe87efd8c81293334b841799b033e2baf5515
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-11-27 16:54:26 +0300

    added

---
+++ DinnerHostingApp.Infrastructure/Persistence/MenuRepository.cs
@@ -0,0 +1,12 @@
+﻿using DinnerHostingApp.Application.Common.Interfaces.Persistence;
+using DinnerHostingApp.Domain.Menus;
+
+namespace DinnerHostingApp.Infrastructure.Persistence;
+public class MenuRepository : IMenuRepository
+{
+    private static readonly List<Menu> _menus = new();
+    public void Add(Menu menu)
+    {
+        _menus.Add(menu);
+    }
+}
